c89d08ee2c64f962c485d287fa64dd6116e274de
asib <ahmedasib1@gmail.com>
2023-10-25 00:20:05 +0600

    send data from json file

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const tShirt = require('./TShirt.json');
 const app = express();
 const port = 5000;
 
 app.get('/',(request, response)=>{
     response.send('yes, i am on')
 });
+
+app.get('/tshirt', (req, res)=>{
+    res.send(tShirt);
+})
+app.get('/tshirt/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const shirt = tShirt.find(shirt => shirt.id === id) || {};
+    res.send(shirt)
+})
+
+
 
 app.listen(port, ()=>{
     console.log('khela hbe', port)
